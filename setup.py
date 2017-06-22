9c757cdf6343f6c09af14162edae1b0c94fe326d
Shenglan Qiao <shenglan@stanford.edu>
2017-06-22 10:07:44 -0700

    setup.py script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='SphericalCow',
+    version='1.0',
+    description='Simulating and analysizing CXS data using spherical harmonics',
+    author='Shenglan Qiao',
+    author_email='shenglan@stanford.edu',
+    url='https://github.com/shenglan0407/SphericalCow.git',
+    packages=['SpericalCow'],
+    package_dir={'SpericalCow':'src'}
+    )
